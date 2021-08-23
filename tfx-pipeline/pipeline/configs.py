2e72e6ab3effec8a9cea6fba9d8737d6d9f54181
Chansung Park <deep.diver.csp@gmail.com>
2021-08-24 00:25:28 +0900

    Update configs.py

--- tfx-pipeline/pipeline/configs.py
+++ tfx-pipeline/pipeline/configs.py
@@ -62,50 +62,6 @@ EVAL_NUM_STEPS = 150
 # Change this value according to your use cases.
 EVAL_ACCURACY_THRESHOLD = 0.6
 
-# Beam args to use BigQueryExampleGen with Beam DirectRunner.
-# TODO(step 7): (Optional) Uncomment here to provide GCP related configs for
-#               BigQuery.
-# BIG_QUERY_WITH_DIRECT_RUNNER_BEAM_PIPELINE_ARGS = [
-#    '--project=' + GOOGLE_CLOUD_PROJECT,
-#    '--temp_location=' + os.path.join('gs://', GCS_BUCKET_NAME, 'tmp'),
-#    ]
-
-# The rate at which to sample rows from the Chicago Taxi dataset using BigQuery.
-# The full taxi dataset is > 120M record.  In the interest of resource
-# savings and time, we've set the default for this example to be much smaller.
-# Feel free to crank it up and process the full dataset!
-_query_sample_rate = 0.0001  # Generate a 0.01% random sample.
-
-# The query that extracts the examples from BigQuery.  The Chicago Taxi dataset
-# used for this example is a public dataset available on Google AI Platform.
-# https://console.cloud.google.com/marketplace/details/city-of-chicago-public-data/chicago-taxi-trips
-# TODO(step 7): (Optional) Uncomment here to use BigQuery.
-# BIG_QUERY_QUERY = """
-#         SELECT
-#           pickup_community_area,
-#           fare,
-#           EXTRACT(MONTH FROM trip_start_timestamp) AS trip_start_month,
-#           EXTRACT(HOUR FROM trip_start_timestamp) AS trip_start_hour,
-#           EXTRACT(DAYOFWEEK FROM trip_start_timestamp) AS trip_start_day,
-#           UNIX_SECONDS(trip_start_timestamp) AS trip_start_timestamp,
-#           pickup_latitude,
-#           pickup_longitude,
-#           dropoff_latitude,
-#           dropoff_longitude,
-#           trip_miles,
-#           pickup_census_tract,
-#           dropoff_census_tract,
-#           payment_type,
-#           company,
-#           trip_seconds,
-#           dropoff_community_area,
-#           tips,
-#           IF(tips > fare * 0.2, 1, 0) AS big_tipper
-#         FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
-#         WHERE (ABS(FARM_FINGERPRINT(unique_key)) / 0x7FFFFFFFFFFFFFFF)
-#           < {query_sample_rate}""".format(
-#    query_sample_rate=_query_sample_rate)
-
 # Beam args to run data processing on DataflowRunner.
 #
 # TODO(b/151114974): Remove `disk_size_gb` flag after default is increased.
