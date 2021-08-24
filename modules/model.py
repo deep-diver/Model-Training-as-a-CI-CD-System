a470954fc80e08bb081dd9ae5ed7e11a68ed7b03
Chansung Park <deep.diver.csp@gmail.com>
2021-08-24 15:19:44 +0000

    experiment changes to model

--- modules/model.py
+++ modules/model.py
@@ -11,11 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""TFX template taxi model.
-
-A DNN keras model which uses features defined in py and network
-parameters defined in py.
-"""
+"""TFX template taxi model."""
 
 from absl import logging
 import tensorflow as tf
